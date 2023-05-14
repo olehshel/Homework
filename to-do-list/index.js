aa14d602dd812851d86c372436b44dfefe42cc8f
olehshel <osheludchenko@gmail.com>
2023-05-14 15:56:19 +0300

    fixes

--- to-do-list/index.js
+++ to-do-list/index.js
@@ -5,21 +5,35 @@ btnAdd.addEventListener('click', (e) => {
     const newLi = document.createElement('li');
     const newSpan = document.createElement('span');
     const newBtn = document.createElement('button');
+    const newInput = document.createElement('input');
 
     newLi.setAttribute('class', 'to-do-list-item');
     newSpan.setAttribute('class', 'to-do-list-item-text');
-    newBtn.classList.add('to-do-list-item-btn');
+    newInput.setAttribute('type', 'checkbox')
+    newInput.classList.add('to-do-list-item-input')
+    newBtn.setAttribute('class', 'to-do-list-item-btn');
 
     const userText = prompt('What is your task ?');
 
     newSpan.textContent = userText;
     newBtn.textContent = 'Delete';
-
-    newSpan.append(newBtn);
+    
+    newLi.append(newInput);
     newLi.append(newSpan);
+    newLi.append(newBtn);
     toDoList.append(newLi);
 
     newBtn.addEventListener('click', () => {
         newLi.remove();
     });
+
+    newInput.addEventListener('change', () => {
+        if (newInput.checked){
+            newSpan.style.opacity = '0.5'
+            newBtn.style.opacity = '0.7'
+        } else {
+            newSpan.style.opacity = '1'
+            newBtn.style.opacity = '1'
+        }
+    })
 });
